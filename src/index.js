d1258f8f4f9e3c3e7019a911db9829fab5bf2c0a
Kirill <stevemcc.man@gmail.com>
2023-11-28 12:37:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-            <BrowserRouter basename="/goit-react-hw-04-image-finder">
-          <App />
-        </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
